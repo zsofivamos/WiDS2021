4a447dc2fc6acb412b1aacc94e7675c1a57048ff
zsofivamos <zsofivamos@gmail.com>
2021-01-31 19:51:35 +0100

    started missing variable exploration

--- EDA.R
+++ EDA.R
@@ -76,10 +76,98 @@ df1 %>%
 
 # observation: all variables either end in _min/_max or _apache. This could be something worth looking into
 
-summary(df1$bun_apache)
-summary(df1$bun_apache)
+blue_zone
 
-# impute the blue zone with the mean values
+## MISSING APACHE SCORES -------------------------------------------
+
+# check the apache related ones first
+missing_apache_scores <- grep("apache",blue_zone, value = TRUE)
+
+df1 %>% 
+  select(missing_apache_scores) %>% 
+  plot_histogram()
+
+# 4 out of 7 apache scores seem to have a distribution skewing to the right, 
+# the rest are closer to normal distribution
+# skewed: bun_apache, creatinine_apache, glucose_apache, wbc_apache
+# normal-ish: hematocrit_apache, sodium_apache, temp_apache
+
+df1 %>% 
+  select(missing_apache_scores) %>% 
+  summary()
+
+# library for most frequent value calculations
+library(modeest)
+
+# check most frequent value ebsides NA 
+mfv(df1$bun_apache, na_rm = T)
+
+# bun_apache: most people seem to be closer to the median value but there are some extreme cases - will impute with the median
+
+mfv(df1$creatinine_apache, na_rm = T)
+
+# creatinine apache: mfv is closer to median, will use that
+
+mfv(df1$glucose_apache, na_rm = T)
+summary(df1$glucose_apache)
+# same for glucose_apache
+
+#let's check one of the normal distributions
+
+summary(df1$temp_apache)
+summary(df1$sodium_apache)
+summary(df1$hematocrit_apache)
+
+# I'll impute the mean values for these ones
+
+# just to check how many columns contain 'apache' lower case 
+# grep("apache",colnames(df1), value = T)
+
+# skewed: bun_apache, creatinine_apache, glucose_apache, wbc_apache
+# let's impute accordingly
+df1$bun_apache <- impute(df1$bun_apache, median)
+
+# check to see if we changed the distribution significantly
+df1$bun_apache %>% plot_histogram()
+
+df1$creatinine_apache <- impute(df1$creatinine_apache, median)
+df1$glucose_apache <- impute(df1$glucose_apache, median)
+df1$wbc_apache <- impute(df1$wbc_apache, median)
+
+# imputing the mean for the rest
+# normal-ish: hematocrit_apache, sodium_apache, temp_apache
+df1$hematocrit_apache <- impute(df1$hematocrit_apache, mean)
+df1$sodium_apache <- impute(df1$sodium_apache, mean)
+df1$temp_apache <- impute(df1$temp_apache, mean)
+
+# missing should be zero
+df1 %>% 
+  select(missing_apache_scores) %>% 
+  plot_missing()
+  
+## MISSING MIN-MAX VALUES --------------------------------------------
+
+missing_min_max <- grep("_min|_max",blue_zone, value = TRUE)
+
+df1 %>% 
+  select(missing_min_max) %>% 
+  plot_missing()
+
+df1 %>% 
+  select(missing_min_max) %>% 
+  plot_histogram()
+
+# df1$d1_glucose_max <- impute(df1$d1_glucose_max, median)
+# df1$d1_glucose_min <- impute(df1$d1_glucose_max, median)
+# df1$d1_bun_max <- impute(df1$d1_bun_max, median)
+# df1$d1_bun_min <- impute(df1$d1_bun_min, median)
+# df1$d1_creatinine_max <- impute(df1$d1_creatinine_max, median)
+# df1$d1_creatinine_min <- impute(df1$d1_creatinine_min, median)
+# df1$d1_platelets_max <- impute(df1$d1_platelets_max, median)
+# df1$d1_platelets_min <- impute(df1$d1_platelets_min, median)
+# df1$d1_wbc_max <- impute(df1$d1_wbc_max, median)
+# df1$d1_wbc_min <- impute(df1$d1_wbc_min, median)
 
 
-impute(df1$bun_apache, mean)
+
+
